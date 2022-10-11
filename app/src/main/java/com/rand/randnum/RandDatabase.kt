2af2b51eaf1ffb60063a99102bb2ebb538804c1f
JoA Ram <kramforu123@sch.ac.kr>
2022-10-12 01:48:25 +0900

    데이터베이스 사용 로직 추가

---
+++ app/src/main/java/com/rand/randnum/RandDatabase.kt
@@ -0,0 +1,34 @@
+package com.rand.randnum
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [RandModel::class], version = 1)
+abstract class RandDatabase : RoomDatabase() {
+
+    abstract fun todoDao(): RandDao
+
+    companion object {
+
+        private var INSTANCE: RandDatabase? = null
+
+        fun getInstance(context: Context): RandDatabase? {
+
+            if (INSTANCE == null) {
+                synchronized(RandDatabase::class) {
+                    INSTANCE = Room.databaseBuilder(  
+                        context.applicationContext,
+                        RandDatabase::class.java,
+                        "tb_cal"
+                    )
+                        .fallbackToDestructiveMigration()
+                        .build()
+                }
+            }
+            return INSTANCE
+        }
+
+    }
+}
